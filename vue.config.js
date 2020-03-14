bd93b5cdd80a50e0e05113692fdc54248551687c
Hung Vu <vunamhung@gmail.com>
2020-03-14 15:44:14 +0700

    :bug: Fixing sourcemap and prependData

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
+    lintOnSave: undefined,
+    productionSourceMap: false,
     configureWebpack: {
         devtool: "eval-source-map",
+    },
+    css: {
+        loaderOptions: {
+            css: {
+                sourceMap: true,
+                importLoaders: 2,
+            },
+            scss: {
+                prependData: `@import "~bootstrap/scss/functions.scss";@import "~@/assets/scss/_variables.scss";`,
+            },
+        },
     },
 };
