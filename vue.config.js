87b8a9e58299a9e4abf7e43af4aa5ad7a840bdf0
Hung Vu <vunamhung@gmail.com>
2020-03-15 11:31:50 +0700

    :recycle: Refactoring code.

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    lintOnSave: "warning",
-    productionSourceMap: false,
-    transpileDependencies: [/\bvue-awesome\b/],
-    configureWebpack: {
-        devtool: "eval-source-map",
+  lintOnSave: "warning",
+  productionSourceMap: false,
+  transpileDependencies: [/\bvue-awesome\b/],
+  configureWebpack: {
+    devtool: "eval-source-map",
+  },
+  css: {
+    sourceMap: true,
+    loaderOptions: {
+      scss: {
+        prependData: `@import "~bootstrap/scss/functions";@import "~@/assets/scss/variables";`,
+      },
     },
-    css: {
-        sourceMap: true,
-        loaderOptions: {
-            scss: {
-                prependData: `@import "~bootstrap/scss/functions";@import "~@/assets/scss/variables";`,
-            },
-        },
-    },
+  },
 };
