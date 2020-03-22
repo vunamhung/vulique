5da0a92531c22485628dcc3e8889c30a09212f64
Hung Vu <vunamhung@gmail.com>
2020-03-23 05:50:12 +0700

    Using project relative image URLs for BootstrapVue custom components

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,28 @@ module.exports = {
   configureWebpack: {
     devtool: "eval-source-map",
   },
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src",
+        };
+
+        return options;
+      });
+  },
   css: {
     sourceMap: true,
     loaderOptions: {
